22a38a1a6a505f96cf883ac751c62be3a84dd6b8
thehxdev <xclimax1324@gmail.com>
2023-06-16 23:39:23 +0330

    Better example with nested vectors

--- data-structures/vector/main.c
+++ data-structures/vector/main.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
+#include <sys/types.h>
 #include "vector.h"
 
+
 int main(void) {
-    vector_t ivec = vec_new(INT64_T);
+    vector_t vec1 = vec_new(STR_T);
+    vector_t vec2 = vec_new(VOID_T);
 
-    for (u_int64_t i = 0; i < 10; i++)
-        vec_append(&ivec, (int64_t*)&i, sizeof(int64_t));
+    /* add string to vec1 */
+    char *str1 = (char *) malloc(32);
+    char *str2 = (char *) malloc(32);
+    strcpy(str1,  "Hello From C!");
+    strcpy(str2, "Generic Vectors");
 
-    int64_t x = 20;
-    vec_insert(&ivec, 0, &x, sizeof(int64_t));
+    vec_append(&vec1, str1, strlen(str1));
+    vec_append(&vec1, str2, strlen(str2));
 
-    vec_sort(&ivec, 0, ivec.length - 1);
+    /* remove original pointers */
+    free(str1);
+    free(str2);
 
-    for (u_int64_t i = 0; i < ivec.length; i++)
-        printf("%ld\n", *(int64_t *)vec_get(&ivec, i));
+    /* add vec1 to vec2
+     * vector in a vector */
+    vec_append(&vec2, &vec1, sizeof(vec1));
+    vector_t *x = (vector_t *) vec_get(&vec2, 0); /* vec1 in vec2 */
+    vec_reverse(x); /* reverse vec1 order */
 
-    vec_delete(&ivec);
+    for (u_int64_t i = 0; i < x->length; i++) {
+        printf("%ld -> %s\n", i, (char*)vec_get(x, i));
+    }
+
+    vec_delete(x); /* delete and free vec1 */
+    vec_delete(&vec2); /* delete and free vec2 */
     return EXIT_SUCCESS;
 }
+

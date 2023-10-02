e9ea4c5e2997fcb5c552f30e925a4e1d70ed8b05
thehxdev <xclimax1324@gmail.com>
2023-10-02 15:13:34 +0330

    added java and c solutions for AOC 2021 day 2

---
+++ challenges/advent-of-code/2021/d2/java-d2/src/main/java/aoc/Main.java
@@ -0,0 +1,65 @@
+package aoc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String programName = "practice";
+        if (args.length != 1) {
+            System.out.printf("Usage: %s <file-path>\n", programName);
+            System.exit(1);
+        }
+
+
+        File inputFile = new File(args[0]);
+        Submarine s1 = new Submarine();
+        Submarine s2 = new Submarine();
+        Scanner fileReader = null;
+
+
+        try {
+            fileReader = new Scanner(inputFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("[ERROR] Could not open file.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+
+        int command_num = 0;
+        while (fileReader.hasNextLine()) {
+            String line = fileReader.nextLine();
+            command_num = extractNumber(line);
+            switch (line.charAt(0)) {
+                case 'f':
+                    s1.horizontal += command_num;
+                    s2.horizontal += command_num;
+                    s2.depth += s2.aim * command_num;
+                    break;
+                case 'u':
+                    s1.depth -= command_num;
+                    s2.aim -= command_num;
+                    break;
+                case 'd':
+                    s1.depth += command_num;
+                    s2.aim += command_num;
+                    break;
+                default:
+                    break;
+            }
+        }
+        fileReader.close();
+
+        System.out.printf("Part 1: %d\n", s1.horizontal * s1.depth);
+        System.out.printf("Part 2: %d\n", s2.horizontal * s2.depth);
+    }
+
+
+    public static int extractNumber(String str) {
+        String[] words = str.split(" ");
+        String num_str = words[1];
+        return Integer.parseInt(num_str);
+    }
+}
---
+++ challenges/advent-of-code/2021/d2/java-d2/src/main/java/aoc/Submarine.java
@@ -0,0 +1,7 @@
+package aoc;
+
+class Submarine {
+    public int horizontal = 0;
+    public int depth = 0;
+    public int aim = 0;
+}

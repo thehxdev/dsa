976dab16ffa0e1f9b0696828ea5ad6a01c6d5a19
thehxdev <xclimax1324@gmail.com>
2024-01-31 21:33:43 +0330

    changed nix flake files to use LLVM

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
-    description = "Nix Flake-Based development environment for C language";
+    description = "Nix Flake-Based development environment for C Language";
 
     inputs = {
-        # use nixos stable channel (currently 23.11)
         nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     };
 
@@ -13,16 +12,26 @@
             pkgs = import nixpkgs { inherit system; };
         in pkgs.mkShell {
             packages = with pkgs; [
-                # compiler and make
-                gcc
+                # compiler and cmake
+                clang_17
+                llvmPackages_17.bintools
                 gnumake
+                cmake
 
-                # debugging
+                # debugging stuff
                 gdb
                 valgrind
+            ];
 
-                # language server
-                ccls
+
+            LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+            BINDGEN_EXTRA_CLANG_ARGS = (builtins.map (a: ''-I"${a}/include"'') [
+              # add dev libraries here (e.g. pkgs.libvmi.dev)
+              pkgs.glibc.dev 
+            ]) ++ [ # Includes with special directory paths
+              ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+              ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+              ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
             ];
 
             # shellHook = ''
@@ -30,3 +39,4 @@
         };
     };
 }
+

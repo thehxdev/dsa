a654d4ee9cb28d77209edb165d62145e1f140e50
thehxdev <xclimax1324@gmail.com>
2024-02-10 01:20:26 +0330

    updated flake.nix for other CPU architectures

--- flake.nix
+++ flake.nix
@@ -1,42 +1,39 @@
 {
-    description = "Nix Flake-Based development environment for C Language";
+    description = "Nix Flake-Based development environment for C";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     };
 
     outputs = { self, nixpkgs, ... }: let
-        system = "x86_64-linux";
+        supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+        forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+        nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in {
-        devShells."${system}".default = let
-            pkgs = import nixpkgs { inherit system; };
-        in pkgs.mkShell {
-            packages = with pkgs; [
-                # compiler and cmake
-                clang_17
-                llvmPackages_17.bintools
-                gnumake
-                cmake
+        devShells = forAllSystems (system: let
+            pkgs = nixpkgsFor.${system};
+        in { 
+            default = pkgs.mkShell {
+                packages = with pkgs; [
+                    ## compiler and cmake
+                    clang_17
+                    llvmPackages_17.bintools
+                    cmake
+                    ninja
+                    gnumake
 
-                # debugging stuff
-                gdb
-                valgrind
-            ];
-
-
-            LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-            BINDGEN_EXTRA_CLANG_ARGS = (builtins.map (a: ''-I"${a}/include"'') [
-              # add dev libraries here (e.g. pkgs.libvmi.dev)
-              pkgs.glibc.dev 
-            ]) ++ [ # Includes with special directory paths
-              ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-              ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-              ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-            ];
-
-            # shellHook = ''
-            # '';
-        };
+                    ## debugging stuff
+                    gdb
+                    valgrind
+                ];
+                
+                shellHook = ''
+                CC=clang
+                CXX=clang++
+                '';
+            };
+        });
     };
 }
-

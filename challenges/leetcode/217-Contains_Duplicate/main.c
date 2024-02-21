17e1d36a8ebabfda083ec6c1ebfb96ef505d44e2
thehxdev <xclimax1324@gmail.com>
2024-02-21 12:45:29 +0330

    solved leetcode problem 217

---
+++ challenges/leetcode/217-Contains_Duplicate/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define size 10
+
+// O(n^2)
+bool containsDuplicate(int* nums, int numsSize) {
+    int i, j, curr;
+    for (int i = 0; i < numsSize; i++) {
+        curr = nums[i];
+        for (int j = i+1; j < numsSize; j++)
+            if (curr == nums[j])
+                return true;
+    }
+    return false;
+}
+
+
+int main(void) {
+    int nums[size] = { 84, 12, 1, 655, 78, 1, 9, 43, 8723, 10 };
+    bool ans = containsDuplicate(nums, size);
+    printf("%d\n", ans);
+    return 0;
+}

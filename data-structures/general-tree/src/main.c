ec1eff09c1ac3124ac49ffcc792415221ac0fc23
thehxdev <xclimax1324@gmail.com>
2024-03-05 19:32:19 +0330

    removed unused variables

--- data-structures/general-tree/src/main.c
+++ data-structures/general-tree/src/main.c
@@ -8,10 +8,10 @@ int main(void) {
     Node *root = tree_add(tree, 10, NULL);
 
     Node *r_ch1 = tree_add(tree, 20, root);
-    Node *r_ch2 = tree_add(tree, 23, root);
-    Node *r_ch3 = tree_add(tree, 56, root);
-    Node *r_ch4 = tree_add(tree, 91, root);
-    Node *ch1_ch1 = tree_add(tree, 10021, r_ch1);
+    (void) tree_add(tree, 23, root);
+    (void) tree_add(tree, 56, root);
+    (void) tree_add(tree, 91, root);
+    (void) tree_add(tree, 10021, r_ch1);
 
     tree_print(tree, root);
     tree_free(tree, root);

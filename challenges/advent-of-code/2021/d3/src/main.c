ff0c09d5370e295d562a8588024624666a5dc176
thehxdev <hossein.khosravi.ce@gmail.com>
2024-08-26 18:46:24 +0330

    better, faster and branch-less solution
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- challenges/advent-of-code/2021/d3/src/main.c
+++ challenges/advent-of-code/2021/d3/src/main.c
@@ -1,46 +1,40 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <math.h>
+#include <errno.h>
 
-#define BUFF_SIZE 32
+// bits count on each line
+#define BIT_COUNT (12)
+#define BUFF_SIZE (BIT_COUNT+2)
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <input-file-path>\n", argv[0]);
-        return 1;
-    }
+	char buff[BUFF_SIZE] = { 0 };
+	int bits_frequescy[BIT_COUNT] = { 0 }, i, lines = 0;
 
-    char buff[BUFF_SIZE] = { 0 };
-    FILE *fp = fopen(argv[1], "r");
-    if (!fp) {
-        fprintf(stderr, "[ERROR] Could not open %s file\n", argv[1]);
-        return 1;
-    }
+	if (argc != 2) {
+		printf("Usage: %s <input>\n", argv[0]);
+		return 1;
+	}
 
-    int bits_frequency[12] = { 0 };
-    while (fgets(buff, BUFF_SIZE, fp)) {
-        for (uint32_t i = 0; i < strlen(buff); i++) {
-            if (buff[i] == '1') {
-                bits_frequency[i] += 1;
-            } else {
-                bits_frequency[i] -= 1;
-            }
-        }
-    }
+	FILE *fp = fopen(argv[1], "r");
+	if (!fp) {
+		perror("could not open input file");
+		return errno;
+	}
 
-    uint16_t decimal_gamma = 0;
-    uint16_t decimal_epsilon = 0;
-    for (int i = 0; i < 12; i++) {
-        if (bits_frequency[i] > 0)
-            bits_frequency[i] = 1;
-        else
-            bits_frequency[i] = 0;
-        decimal_gamma += (bits_frequency[i] * ((int) pow(2, 11 - i)));
-    }
-    decimal_epsilon = ((~decimal_gamma) & (0xfff));
-    printf("Part 1: %d\n", decimal_gamma * decimal_epsilon);
+	while ((fgets(buff, BUFF_SIZE, fp)) != NULL) {
+		for (i = 0; i < BIT_COUNT; i++)
+			// just count ones
+			bits_frequescy[i] += (buff[i] - '0');
+		lines += 1;
+	}
+	fclose(fp);
 
-    fclose(fp);
-    return 0;
+	short gamma_rate = 0;
+	short epsilon_rate = 0;
+	for (i = 0; i < BIT_COUNT; i++) {
+		gamma_rate |= ((bits_frequescy[i] > (lines/2)) << (BIT_COUNT-i-1));
+	}
+	epsilon_rate = ((~gamma_rate) & (0xfff));
+
+	printf("Part 1: %d\n", gamma_rate * epsilon_rate);
+	return 0;
 }
